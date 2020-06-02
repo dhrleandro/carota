f930032419a5f2ee987f03a90beeb1334c91e079
thisunravisara <thisunravisara@gmail.com>
2020-06-02 13:12:54 +0530

    Used one if block

--- src/doc.js
+++ src/doc.js
@@ -94,18 +94,19 @@ var prototype = node.derive({
          * Carota adds a new word with a new line to mark end-of-document and it's height is not specified
          * the height should equal to the previous line height
          */
-        if( this.words && this.words[ this.words.length - 1 ] && this.words[ this.words.length - 2 ] ) {
-            if ( this.words[ this.words.length - 1 ].text.plainText === '\n' ) {
-                var prev = this.words[ this.words.length - 2 ];
-                var cur = this.words[ this.words.length - 1 ];
+        if( this.words && this.words[ this.words.length - 1 ]
+            && this.words[ this.words.length - 2 ]
+            && this.words[ this.words.length - 1 ].text.plainText === '\n'
+        ) {
+            var prev = this.words[ this.words.length - 2 ];
+            var cur = this.words[ this.words.length - 1 ];
 
-                // word properties are created by cloning using Object.create() and ascent and descent 
-                // properties are readonly, so have to delete and set.
-                delete cur.ascent;
-                cur.ascent = prev.ascent;
-                delete cur.descent;
-                cur.descent = prev.descent;
-            }
+            // word properties are created by cloning using Object.create() and ascent and descent 
+            // properties are readonly, so have to delete and set.
+            delete cur.ascent;
+            cur.ascent = prev.ascent;
+            delete cur.descent;
+            cur.descent = prev.descent;
         }
         
         this.layout();
