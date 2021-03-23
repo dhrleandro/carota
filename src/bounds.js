19efe396b86b8c0d6a70aa34dc78d2cf02e5129d
Lina Refai <linatrefai@yahoo.com>
2021-03-23 16:33:08 +0530

    updated bounds function to round up to 2 dp by default

--- src/bounds.js
+++ src/bounds.js
@@ -1,10 +1,14 @@
 var doc = require('./doc');
 
-exports = module.exports = function( data, width, styles ) {
+exports = module.exports = function( data, width, styles, roundByOffset = 0.005, roundTo = 2 ) {
     var canvas = document.createElement( 'canvas' );
     var carota = doc( styles );
     carota.load( data );
     carota.width( width );
     carota.draw( canvas.getContext( '2d' ));
-    return { width: carota.frame.actualWidth(), height: carota.frame.actualHeight(), minWidth: carota.frame.minWidth() };
+    var width = carota.frame.actualWidth();
+    var height = carota.frame.actualHeight();
+    var roundedWidth = +(( width + roundByOffset ).toFixed( roundTo ));
+    var roundedHeight = +(( height + roundByOffset ).toFixed( roundTo )); 
+    return { width: roundedWidth, height: roundedHeight, minWidth: carota.frame.minWidth() };
 };
