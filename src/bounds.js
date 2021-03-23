a9133fbf37c2599a40533049f2f159629e389daa
Lina Refai <linatrefai@yahoo.com>
2021-03-23 19:38:55 +0530

    rounded minWidth as well

--- src/bounds.js
+++ src/bounds.js
@@ -7,8 +7,10 @@ exports = module.exports = function( data, width, styles, roundByOffset = 0.005,
     carota.width( width );
     carota.draw( canvas.getContext( '2d' ));
     var width = carota.frame.actualWidth();
-    var height = carota.frame.actualHeight();
     var roundedWidth = +(( width + roundByOffset ).toFixed( roundTo ));
+    var height = carota.frame.actualHeight();
     var roundedHeight = +(( height + roundByOffset ).toFixed( roundTo )); 
-    return { width: roundedWidth, height: roundedHeight, minWidth: carota.frame.minWidth() };
+    var minWidth = carota.frame.minWidth();
+    var roundedMinWidth = +(( minWidth+ roundByOffset ).toFixed( roundTo )); 
+    return { width: roundedWidth, height: roundedHeight, minWidth: roundedMinWidth };
 };
