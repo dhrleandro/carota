7c1c07586ac8641dd826fee47d79260e804f5914
Leandro Daher <117833992+dhrleandro@users.noreply.github.com>
2024-02-23 12:27:21 -0300

    fix: prepublish

--- make.js
+++ make.js
@@ -1,21 +1,25 @@
-var webmake = require('webmake');
-var minify = require('node-minify');
+const webmake = require('webmake');
+const minify = require('@node-minify/core');
+const uglifyjs = require('@node-minify/uglify-js');
+
+function runMinify() {
+    minify({
+        compressor: uglifyjs,
+        input: ['carota-debug.js'],
+        output: 'carota-min.js',
+        callback: function (err, min) {
+            if (err) {
+                console.log(err);
+            }
+        }
+    });
+}
 
 webmake('src/carota.js', { output: 'carota-debug.js' }, function(result) {
     if (!result) {
+        runMinify();
         console.log('All good');
     } else {
         console.log(result);
-    }
-});
-
-new minify.minify({
-    type: 'uglifyjs',
-    fileIn: 'carota-debug.js',
-    fileOut: 'carota-min.js',
-    callback: function(err, min){
-        if (err) {
-            console.log(err);
-        }
     }
 });
